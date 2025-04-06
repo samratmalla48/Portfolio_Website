8d681be3da4e837683a70cb57e625397bab9718d
Samrat Malla <samratmalla48@gmail.com>
2025-04-07 02:25:51 +1000

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Samrat Malla Software Engineer Resume.pdf";
+import pdf from "../../Assets/../Assets/Samrat_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
